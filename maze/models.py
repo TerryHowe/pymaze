f99818bf5e9f058fcfc034d9065e135358d8c303
Terry Howe <terrylhowe@gmail.com>
2017-05-05 09:09:24 -0600

    Attempt at passage

--- maze/models.py
+++ maze/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+DIRECTION = (
+	('N', 'North'),
+	('E', 'East'),
+	('S', 'South'),
+	('W', 'West'),
+)
+
+class Passage(models.Model):
+	room_x = models.IntegerField(primary_key=True)
+	room_y = models.IntegerField(primary_key=True)
+	direction = models.CharField(max_length=1, choices=DIRECTION, primary_key=True)
+	destination = models.ForeignKey('self')
