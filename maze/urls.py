6dbe4f59a85b38b447a33e726719006c720cb606
Terry Howe <terrylhowe@gmail.com>
2017-05-05 13:15:17 -0600

    add rooms view

--- maze/urls.py
+++ maze/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^(?P<room_x>[0-9]+)/(?P<room_y>[0-9]+)/(?P<direction>[NESW])$', views.room, name='room'),
 ]
--- maze/views.py
+++ maze/views.py
@@ -9,3 +9,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the maze.")
+
+def room(request, room_x, room_y, direction):
+    return HttpResponse("Looking %s from %s, %s." % (direction, room_x, room_y))
