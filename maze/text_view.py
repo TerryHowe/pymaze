b46984228aaf0844df9f237663441c40dfb57a5a
Terry Howe <terrylhowe@gmail.com>
2017-05-06 09:25:49 -0600

    forward right wall

--- maze/text_view.py
+++ maze/text_view.py
@@ -242,7 +242,6 @@ class TextView(object):
 		if left_room:
 			if not left_room.go_forward(direction):
 				t.append(LEFT_FORWARD_RIGHT)
-			pass
 		else:
 			t.append(LEFT)
 		if forward_room:
@@ -250,17 +249,17 @@ class TextView(object):
 			if forward_left_room:
 				if not forward_left_room.go_forward(direction):
 					t.append(FORWARD_LEFT_FORWARD_RIGHT)
-				pass
 			else:
 				t.append(FORWARD_LEFT)
 			if not forward_room.go_forward(direction):
 				t.append(FORWARD_FORWARD)
 			forward_right_room = forward_room.go_right(direction)
-			#if forward_right_room:
+			if forward_right_room:
 			#	if not forward_right_room.get(direction, None):
 			#		t.append(FORWARD_RIGHT_FORWARD_LEFT)
-			#else:
-			#	t.append(FORWARD_RIGHT)
+				pass
+			else:
+				t.append(FORWARD_RIGHT)
 			pass
 		else:
 			t.append(FORWARD)
