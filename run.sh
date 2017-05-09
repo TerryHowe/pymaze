b4022056498ce2983e4c0f96838104ae21d418ce
Terry Howe <terrylhowe@gmail.com>
2017-05-09 14:53:33 -0600

    simplify run

--- run.sh
+++ run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-python manage.py migrate
-python manage.py collectstatic --noinput
-
 LOG_DIR='.'
 ERROR_LOG="${LOG_DIR}/error.log"
 ACCESS_LOG="${LOG_DIR}/access.log"
-
-touch ${ERROR_LOG}
-touch ${ACCESS_LOG}
-tail -n 0 -f ${ERROR_LOG} &
-
 gunicorn pymaze.wsgi:application \
 	--name pymaze \
 	--bind 0.0.0.0:8000 \
