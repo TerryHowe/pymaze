e08315d49d2b5771728809903e790691d75dde02
Terry Howe <terrylhowe@gmail.com>
2017-05-09 08:43:08 -0600

    work on run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-python manage.py runserver
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+LOG_DIR='.'
+ERROR_LOG="${LOG_DIR}/error.log"
+ACCESS_LOG="${LOG_DIR}/access.log"
+
+touch ${ERROR_LOG}
+touch ${ACCESS_LOG}
+tail -n 0 -f ${ERROR_LOG} &
+
+gunicorn pymaze.wsgi:application \
+	--name pymaze \
+	--bind 0.0.0.0:8000 \
+	--workers 3 \
+	--log-level=info \
+	--log-file=${ERROR_LOG} \
+	--access-logfile=${ACCESS_LOG} \
+	"$@"
